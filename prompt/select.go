42c7a2c15002dec703a0df46119fe222b4001ce7
Josa Gesell <josa@gesell.me>
2018-08-24 12:45:15 +0200

    🐛  Fix Select not using label argument

--- prompt/select.go
+++ prompt/select.go
@@ -6,7 +6,7 @@ import "github.com/josa42/go-prompt/elements"
 func Select(label string, options Options) (selection string, canceled bool) {
 
 	menu := elements.Select{
-		Label: "Select options",
+		Label: label,
 		Multi: false,
 	}
 
